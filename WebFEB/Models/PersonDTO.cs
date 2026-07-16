9ed0aa50db6eb1b0f2337e79adbd7dc70e8c4688
FRANK <developer71@azet.sk>
2026-07-16 21:25:45 +0200

    refactor: validate and initialize person data

--- WebFEB/Models/PersonDTO.cs
+++ WebFEB/Models/PersonDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace WebFEB.Models
-{
-    public class PersonDTO
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public double Credit { get; set; }
+using System.ComponentModel.DataAnnotations;
 
-    }
+namespace WebFEB.Models;
+
+public class PersonDTO
+{
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    public double Credit { get; set; }
 }
